f07e390692aaf316f9a1153b682a58fbcb79ee0f
arosalob <arosalob@g.educaand.es>
2025-03-21 13:00:37 +0100

    Html y css en beta, inicio de js

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -1,1 +1,13 @@
-//Escribe aquí tu código JavaScript
+let nombre = document.getElementById("nombre");
+let apellidos = document.getElementById("apellidos");
+let email = document.getElementById("email");
+let direccion = document.getElementById("direccion");
+let telefono = document.getElementById("telefono");
+
+let formulario = document.getElementById("formulario");
+
+formulario.onsubmit = function(event) {
+    if (false) {
+        event.preventDefault();
+    }
+}
